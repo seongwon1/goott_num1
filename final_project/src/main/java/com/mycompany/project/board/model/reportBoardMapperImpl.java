e745ed1f8cef921ee7925a5463894f218f1449d9
HGucci <ekdrnekd2@naver.com>
2022-10-04 17:30:45 +0900

    10/04 17:30 kim hyun gu

---
+++ final_project/src/main/java/com/mycompany/project/board/model/reportBoardMapperImpl.java
@@ -0,0 +1,34 @@
+package com.mycompany.project.board.model;
+
+import java.util.List;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class reportBoardMapperImpl implements reportBoardMapper {
+
+	@Autowired
+	SqlSessionTemplate sqlsession;
+	
+	@Override
+	public List<reportBoardDTO> list() {
+		
+		return sqlsession.selectList("reportBoardMapper.list");
+	}
+
+	@Override
+	public List<reportBoardDTO> listPaging(reportBoardCriteria cri) {
+		
+		return sqlsession.selectList("reportBoardMapper.listPaging", cri);
+	}
+
+	@Override
+	public int total() {
+		
+		return sqlsession.selectOne("reportBoardMapper.total");
+	}
+
+
+}
