3bdde9534b92dd44ef8d8ec90ebbdd67e67c64c2
skypy1214 <skypy1214@gmail.com>
2022-10-06 06:14:32 +0900

    eee

---
+++ final_project/src/main/java/com/mycompany/project/common/controller/AdminController.java
@@ -0,0 +1,51 @@
+package com.mycompany.project.common.controller;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.mycompany.project.common.model.dto.MemberDTO;
+import com.mycompany.project.common.service.MemberService;
+
+@RequestMapping("/admin")
+@Controller
+public class AdminController {
+	
+	@Inject
+	MemberService service;
+	
+	@RequestMapping("/main")
+	public String main(Model model) {
+		model.addAttribute("userList", service.userList());
+		return "/admin/main";
+	}
+	
+	@RequestMapping("/userDetail")
+	public String userDetail(Model model, MemberDTO dto) {
+		model.addAttribute("user", service.userDetail(dto));
+		return "/admin/userDetail";
+	}
+	
+	
+
+	@RequestMapping(value="/pause", method=RequestMethod.POST)
+	public String pause(MemberDTO dto) {
+		service.userPause(dto);
+		
+		return "redirect:/admin/main";
+	}
+	@RequestMapping(value="/userDel", method=RequestMethod.POST)
+	public String userDel(MemberDTO dto) {
+		service.userDel(dto);
+		
+		return "redirect:/admin/main";
+	}
+	
+	
+}
---
+++ final_project/src/main/java/com/mycompany/project/common/model/dto/MemberDTO.java
@@ -0,0 +1,94 @@
+package com.mycompany.project.common.model.dto;
+
+public class MemberDTO {
+	private String user_id;
+	private String user_password;
+	private String user_name;
+	private String address;
+	private String address_detail;
+	private String zipcode;
+	private String enabled_YN;
+	private String authority;
+	private String pause_date; 
+	private String email;
+	private int count;
+	private String user_state;
+	
+	public String getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(String user_id) {
+		this.user_id = user_id;
+	}
+	public String getUser_password() {
+		return user_password;
+	}
+	public void setUser_password(String user_password) {
+		this.user_password = user_password;
+	}
+	public String getUser_name() {
+		return user_name;
+	}
+	public void setUser_name(String user_name) {
+		this.user_name = user_name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getAddress_detail() {
+		return address_detail;
+	}
+	public void setAddress_detail(String address_detail) {
+		this.address_detail = address_detail;
+	}
+	public String getZipcode() {
+		return zipcode;
+	}
+	public void setZipcode(String zipcode) {
+		this.zipcode = zipcode;
+	}
+	public String getEnabled_YN() {
+		return enabled_YN;
+	}
+	public void setEnabled_YN(String enabled_YN) {
+		this.enabled_YN = enabled_YN;
+	}
+	public String getAuthority() {
+		return authority;
+	}
+	public void setAuthority(String authority) {
+		this.authority = authority;
+	}
+	public String getPause_date() {
+		return pause_date;
+	}
+	public void setPause_date(String pause_date) {
+		this.pause_date = pause_date;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public int getCount() {
+		return count;
+	}
+	public void setCount(int count) {
+		this.count = count;
+	}
+	public String getUser_state() {
+		return user_state;
+	}
+	public void setUser_state(String user_state) {
+		this.user_state = user_state;
+	}
+	
+	
+	
+	
+
+}
